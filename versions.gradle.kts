4676fefb297ac136f0f1e0486e4971cd8f03b18d
Alexander Weickmann <alexander.weickmann@link-time.com>
2019-08-09 10:56:42 +0200

    gradle node plugin 2.0.0

--- versions.gradle.kts
+++ versions.gradle.kts
@@ -2,7 +2,7 @@ extra.apply {
     // gradle plugin dependencies
     set("gradle_credentials_plugin_version", "1.0.7")
     set("dokka_gradle_plugin_version", "0.9.18")
-    set("gradle_node_plugin_version", "1.5.3")
+    set("gradle_node_plugin_version", "2.0.0")
     set("gradle_nexus_plugin_version", "2.3.1")
     set("gradle_nexus_staging_plugin_version", "0.21.0")
     set("kotlin_frontend_plugin_version", "0.0.45")
